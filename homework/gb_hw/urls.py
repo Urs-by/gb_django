0c78ed37e8c0aac2a33cbd7626195e73c6453841
Artur.by <urs.by@iclood.com>
2024-01-24 20:33:48 +0300

    homework3 almost solved

--- homework/gb_hw/urls.py
+++ homework/gb_hw/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
-from .views import Order, ProductList
+from .views import OrderList, ProductList, CustomerList, OrderDetail, OrdersCustomer
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('orders/', Order.as_view(), name='orders'),
     path('products/', ProductList.as_view(), name='products'),
-    # path('orders/<uuid:pk>/', order_detail)
+    path('orders/', OrderList.as_view(), name='orders'),
+    path('orders/<int:order>/', OrderDetail.as_view(), name='orders'),
+    path('customers/', CustomerList.as_view(), name='customers'),
+    path('customer_detail/<int:customer>/', OrdersCustomer.as_view(), name='customer_detail'),
+    path('order_detail/<int:order>/', OrderDetail.as_view(), name='order_detail'),
+
 ]
+
--- homework/gb_hw/views.py
+++ homework/gb_hw/views.py
@@ -3,12 +3,14 @@ from django.views import View
 from django.shortcuts import render, get_object_or_404
 import logging
 from .models import Customer, Product, Order
+from datetime import datetime, timedelta
+
 logger = logging.getLogger(__name__)
 
 
 def index(request):
     logger.info('Index page accessed')
-    return HttpResponse("<h1> Мой первый dgango проект. </h1>")
+    return render(request, "gb_hw/base.html")
 
 
 def about(request):
@@ -28,20 +30,64 @@ def about(request):
         return HttpResponse("Oops, something went wrong.")
 
 
-class Customer(View):
+class CustomerList(View):
     def get(self, request):
-        return render(request, 'customer.html')
+        customers = Customer.objects.all()
+
+        context = {'customers': customers}
+        return render(request, 'gb_hw/customers.html', context)
 
 
 class ProductList(View):
     def get(self, request):
         products = Product.objects.all()
         context = {'products': products}
-        context = {'products': products}
         return render(request, 'gb_hw/products.html', context)
 
 
-class Order(View):
+class OrderList(View):
     def get(self, request):
-        return render(request, 'order.html')
+        # почему-то не работает get_object_or_404 в данном случае
+        # orders = get_object_or_404(Order, pk=order)
 
+        orders = Order.objects.all()
+
+        context = {'orders': orders}
+        return render(request, 'gb_hw/orders.html', context)
+
+
+class OrderDetail(View):
+    def get(self, request, order):
+        orders = Order.objects.filter(pk=order)
+        for i in orders:
+            order_products = i.product.all()
+            context = {
+                'order': i,
+                'order_products': order_products
+            }
+
+        return render(request, 'gb_hw/order_detail.html', context)
+
+
+class OrdersCustomer(View):
+    def get(self, request, customer):
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=360)
+
+        customer = Customer.objects.get(pk=customer)
+        orders = Order.objects.filter(customer_id=customer.pk, date_ordered__range=(start_date, end_date)).order_by(
+            '-date_ordered')[:2]
+
+        products_total = set()
+        for order in orders:
+            product = order.product.all()
+            for i in product:
+                products_total.add(i)
+
+        context = {
+            'customer': customer,
+            'orders': orders,
+            'products': products_total
+        }
+
+        return render(request, 'gb_hw/customer_detail.html', context)
